d8b996d126d96aa19ce33ba02bae2144adf89e15
voizun <83576282+voizun@users.noreply.github.com>
2024-12-24 16:39:43 +0900

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
+import pillow_avif
 
 # スクリーンショット保存先フォルダ
 output_dir = "screenshots"
